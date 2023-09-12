b7613b03badfa70b3600b0e4b154f2a51edc981a
Daniel Ding <41363844+BioniCosmos@users.noreply.github.com>
2023-09-12 19:52:53 +0800

    Remove unused links

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -6,16 +6,6 @@ export default defineUserConfig({
   description: '大学生科技创新实验室',
   head: [['link', { rel: 'icon', href: '/images/logo.webp' }]],
   theme: defaultTheme({
-    navbar: [
-      {
-        text: '博客',
-        link: 'https://blog.ustillab.com',
-      },
-      {
-        text: '论坛',
-        link: 'https://bbs.ustillab.com',
-      },
-    ],
     logo: '/images/logo.webp',
   }),
 })
